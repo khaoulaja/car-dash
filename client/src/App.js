6b3884826b692d449d73680940d1a9312bb108f8
khaoula <khaoulajabour4@gmail.com>
2022-03-18 01:05:03 -0400

    add responsive login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
 
@@ -8,7 +9,7 @@ function App() {
   return (
     <div>
     <Header/>
-    <Signup/>
+    <Login/>
     <Footer/>
     </div>
   );
