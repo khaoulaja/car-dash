8edb1ff9948a7a84d291b9048ff83b1d9aafb86e
khaoula <khaoulajabour4@gmail.com>
2022-03-18 23:12:53 -0400

    responsive landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,13 +4,20 @@ const Home =()=>{
     return (
         // bg-slate-50
         <div className="min-h-screen md:flex bg-hero ">
-            <div className="overflow-hidden mt-32 mx-10 md:ml-20 flex flex-col text-center text-slate-900 items-start md:text-left md:w-1/2 py-10 ">
-                <h1 className="text-2xl md:text-5xl font-bold mb-7"> Track repairs and services needed for all your cars</h1>
+            <div className="overflow-hidden pt-24 md:pt-44 mx-10 md:ml-20 flex flex-col text-slate-900 items-start md:text-left md:w-1/2 py-10 ">
+                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-7 capitalize">want to track repairs and services needed for all your cars?</h1>
                 <p className="text-xl font-medium text-gray-800">That’s a lot to add to your already busy calendar. 
-                <br/>Luckily, there is an easier way to keep track of everything</p>
-                <button>Resister now</button>
+                <br/>Luckily, there is an easier way to keep track of everything. 
+                <br/>
+                 <span className="font-bold">Car-Dash</span> will let you do just that. <br/>
+                Sign up now and keep your vehicle serviced and in tip-top shape!
+                </p>
+                <button type="submit" 
+                    className="block w-1/2 my-8 py-2 rounded-full text-white font-semibold mb-2 bg-gradient-to-l from-sky-500 to-indigo-600 transition duration-500 hover:shadow-md hover:hue-rotate-30 ">
+                        Register now
+                </button>
             </div>
-            <div className="shrink-0 overflow-hidden md:flex w-1/2 i justify-around items-center">
+            <div className="shrink-0 overflow-hidden md:flex w-full md:w-1/2 i justify-around items-center">
              <img className="h-auto" src={pic}/>
             </div>
         </div>
