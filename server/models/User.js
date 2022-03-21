7a245df9ebc2a29924a089b418b083b337f0aeee
khaoula <khaoulajabour4@gmail.com>
2022-03-21 02:37:50 -0400

    add cars array to user schema

--- server/models/User.js
+++ server/models/User.js
@@ -20,6 +20,12 @@ const userSchema = new Schema(
             required: true,
             minlength: 5
         },
+        cars: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Car'
+            }
+        ]
 
     },
     {
