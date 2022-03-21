fd721db10e7266257cebdbcc344ed86662569a9a
khaoula <khaoulajabour4@gmail.com>
2022-03-21 03:22:44 -0400

    add mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,70 @@
+import{ gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!){
+        login(email: $email, password: $password){
+            token
+            user{
+                _id
+                username
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_CAR =gql`
+mutation($name: String!, $make: String!, $model: String!, $year: String!, $color: String!){
+    addCar(name: $name, make: $make, model: $model, year: $year, color: $color) {
+      _id
+      name
+      make
+      model
+      year 
+      color
+      image
+      maintenanceCount
+      maintenance
+    }
+  }
+`;
+export const DELETE_CAR =gql`
+mutation($carId: ID!){
+    deleteCar(carId: $carId) {
+        _id
+        name
+    }
+  }
+`;
+
+export const SAVE_MAINTENANCE =gql`
+mutation($carId: ID!, $type: [String]){
+    saveMaintenance(carId: $carId, type: $type) {
+        _id
+        name
+        maintenanceCount
+        maintenance
+    }
+  }
+`;
+export const REMOVE_MAINTENANCE = gql`
+mutation($carId: ID!, $type: String!){
+    removeMaintenance(carId: $carId, type: $type) {
+        _id
+        name
+        maintenanceCount
+        maintenance      
+    }
+  }
+`;
